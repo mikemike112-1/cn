ed788c3ee3ccc066e28c4eb61b9860e285294f6b
unknown <mmhond1@students.towson.edu>
2021-12-29 18:22:55 -0500

    activity 1 comments basically done

--- Activity 2/main.py
+++ Activity 2/main.py
@@ -46,11 +46,35 @@ pygame.init()
 #300 is the x axis and 400 is the y axis
 DISPLAYSURF = pygame.display.set_mode((400, 300))
 
-
+#as you can tell now, pygame.display deals with everything that is your game
+#window.
+#pygame.display.set_caption sets the name for your game window
+#at the end, change and run the words in the quotes a few times
 pygame.display.set_caption('Hello World!')
+
+#just like in javascript, we also have a while-true loop in python,
+#it looks like this...
 while True: # main game loop
+
+    #this, is a for loop that will automatically loop through all the
+    #elements inside of the given list or array of stuff
     for event in pygame.event.get():
+
+        #if statements are written similar to the following to the following
+        #example
+        #the "event.type" is referring to the type of event that is happening
+        #the "QUIT" event happens when you clik on the x to leave the game
         if event.type == QUIT:
+
+            #this will end all the pygame uses in the code
             pygame.quit()
+
+            #this will exit the python window cleanly and safely
             sys.exit()
+
+    #another pygame.display section of code
+    #this will apply all the changes you made to the screen and actually
+    #display them
+    #since we did not make any changes to our screen, we will continue to
+    #see a blank screen
     pygame.display.update()
