1c9e01423ad45dda6d9c15893c7993fc3dfc2837
michael <michaelmhonda@gmail.com>
2022-02-28 04:10:40 -0500

    act 5 pys idea added

---
+++ brown belt/activity 5/pys.py
@@ -0,0 +1,11 @@
+"""
+Should this one be a pys?
+
+If so, we can ask the students to create a game which has a rectangle that moves around using the mouse then have it
+change color once it hits a specific place on the screen.
+
+It could have a second part as well where the student takes this same idea and instead has to use the arrow keys.
+
+Something we may want to consider is whether the student should be given some of the code as starting code and left
+with blanks that they need to complete themselves.
+"""
